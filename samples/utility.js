3af52e3ca71d69a4f2db773c484101b5b995362e
Patrick Cozzi <pjcozzi@gmail.com>
2015-12-24 11:00:26 -0500

    Move helpers to separate file

---
+++ samples/utility.js
@@ -0,0 +1,31 @@
+(function () {
+    'use strict';
+
+    window.getShaderSource = function(id) {
+        return document.getElementById(id).textContent.replace(/^\s+|\s+$/g, '');
+    }
+
+    function createShader(gl, source, type) {
+        var shader = gl.createShader(type);
+        gl.shaderSource(shader, source);
+        gl.compileShader(shader);
+        return shader;
+    }
+
+    window.createProgram = function(gl, vertexShaderSource, fragmentShaderSource) {
+        var program = gl.createProgram();
+        var vshader = createShader(gl, vertexShaderSource, gl.VERTEX_SHADER);
+        var fshader = createShader(gl, fragmentShaderSource, gl.FRAGMENT_SHADER);
+        gl.attachShader(program, vshader);
+        gl.deleteShader(vshader);
+        gl.attachShader(program, fshader);
+        gl.deleteShader(fshader);
+        gl.linkProgram(program);
+
+        console.log(gl.getProgramInfoLog(program));
+        console.log(gl.getShaderInfoLog(vshader));
+        console.log(gl.getShaderInfoLog(fshader));
+
+        return program;
+    }
+})();
