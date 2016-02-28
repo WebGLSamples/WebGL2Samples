0ab7a30a0fe94bc69be86d4e1b62c84ca6873205
shrekshao <shrekshao@gmail.com>
2016-02-28 16:11:06 -0500

    count, weird 18 offset

--- samples/third-party/gltf-loader.js
+++ samples/third-party/gltf-loader.js
@@ -204,6 +204,7 @@
             
             var stride;
             var offset;
+            var count;
             
             // apply transformations
             // attributes are interleaved
@@ -213,37 +214,65 @@
 
                 if (semantic.substring(0, 8) === 'POSITION') {
                     stride = scene.positionByteStride / AttributeSize[attribute.componentType];
-                    offset = scene.positionByteOffset / AttributeSize[attribute.componentType];
-                    for (var i = 0; i < data.length; i += stride) {
+                    offset = 18 + scene.positionByteOffset / AttributeSize[attribute.componentType];
+                    count = attribute.count;
+                    // for (var i = 0; i < data.length; i += stride) {
+                    //     // @todo: add vec2 and other(needed?) support
+                    //     if(scene.positionNumberOfComponents === 3) {
+                    //         vec4.set(tmpVec4, data[i + offset]
+                    //                         , data[i + offset + 1]
+                    //                         , data[i + offset + 2]
+                    //                         , 1);
+                    //         vec4.transformMat4(tmpVec4, tmpVec4, matrix);
+                    //         vec4.scale(tmpVec4, tmpVec4, tmpVec4[3]);
+                    //         data[i + offset] = tmpVec4[0];
+                    //         data[i + offset + 1] = tmpVec4[1];
+                    //         data[i + offset + 2] = tmpVec4[2];
+                    //     }
+                    // }
+                    for (var i = 0; i < count; ++i) {
                         // @todo: add vec2 and other(needed?) support
                         if(scene.positionNumberOfComponents === 3) {
-                            vec4.set(tmpVec4, data[i + offset]
-                                            , data[i + offset + 1]
-                                            , data[i + offset + 2]
+                            vec4.set(tmpVec4, data[stride * i + offset]
+                                            , data[stride * i + offset + 1]
+                                            , data[stride * i + offset + 2]
                                             , 1);
                             vec4.transformMat4(tmpVec4, tmpVec4, matrix);
                             vec4.scale(tmpVec4, tmpVec4, tmpVec4[3]);
-                            data[i + offset] = tmpVec4[0];
-                            data[i + offset + 1] = tmpVec4[1];
-                            data[i + offset + 2] = tmpVec4[2];
+                            data[stride * i + offset] = tmpVec4[0];
+                            data[stride * i + offset + 1] = tmpVec4[1];
+                            data[stride * i + offset + 2] = tmpVec4[2];
                         }
                     }
                 } else if (semantic.substring(0, 6) === 'NORMAL') {
                     stride = scene.normalByteStride / AttributeSize[attribute.componentType];
-                    offset = scene.normalByteOffset / AttributeSize[attribute.componentType];
+                    offset = 18 + scene.normalByteOffset / AttributeSize[attribute.componentType];
+                    count = attribute.count;
                     mat4.invert(inverseTransposeMatrix, matrix);
                     mat4.transpose(inverseTransposeMatrix, inverseTransposeMatrix);                    
-                    for (var i = 0; i < data.length; i += stride) {
+                    // for (var i = 0; i < data.length; i += stride) {
+                    //     // @todo: add vec2 and other(needed?) support
+                    //     vec4.set(tmpVec4, data[i + offset]
+                    //                     , data[i + offset + 1]
+                    //                     , data[i + offset + 2]
+                    //                     , 0);
+                    //     vec4.transformMat4(tmpVec4, tmpVec4, inverseTransposeMatrix);
+                    //     vec4.normalize(tmpVec4, tmpVec4);
+                    //     data[i + offset] = tmpVec4[0];
+                    //     data[i + offset + 1] = tmpVec4[1];
+                    //     data[i + offset + 2] = tmpVec4[2];
+                    // }
+                    for (var i = 0; i < count; ++i) {
                         // @todo: add vec2 and other(needed?) support
-                        vec4.set(tmpVec4, data[i + offset]
-                                        , data[i + offset + 1]
-                                        , data[i + offset + 2]
+                        vec4.set(tmpVec4, data[stride * i + offset]
+                                        , data[stride * i + offset + 1]
+                                        , data[stride * i + offset + 2]
                                         , 0);
                         vec4.transformMat4(tmpVec4, tmpVec4, inverseTransposeMatrix);
                         vec4.normalize(tmpVec4, tmpVec4);
-                        data[i + offset] = tmpVec4[0];
-                        data[i + offset + 1] = tmpVec4[1];
-                        data[i + offset + 2] = tmpVec4[2];
+                        data[stride * i + offset] = tmpVec4[0];
+                        data[stride * i + offset + 1] = tmpVec4[1];
+                        data[stride * i + offset + 2] = tmpVec4[2];
                     }
                 }
             }
